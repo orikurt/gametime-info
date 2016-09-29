ac63cf92f0922a9eb679b3629ff504413d098046
orikurt <orikball@gmail.com>
2016-09-29 19:29:46 +0100

    file structure and more info

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var favicon = require('serve-favicon');
 
 var app = express();
 
-app.use(express.static('.'));
-app.use(favicon(__dirname + '/img/favicon.ico'));
+app.use(express.static('static'));
+app.use(favicon(__dirname + '/static/img/favicon.ico'));
 
 app.listen(3000, function(){
 	console.log('server is listening');
