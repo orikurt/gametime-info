b1e3976e64d31dfe2beedb944bf33b65c8e73149
orikurt <orikball@gmail.com>
2016-10-03 14:01:38 +0100

    server config file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var favicon = require('serve-favicon');
+var conf = require('./static/config');
 
 var app = express();
 
 app.use(express.static('static'));
 app.use(favicon(__dirname + '/static/img/favicon.ico'));
 
-app.listen(3000, function(){
+app.listen(conf.server.port, function(){
 	console.log('server is listening');
 });
